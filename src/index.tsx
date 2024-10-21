068412a74542a5dbf883932513f27f663fbee39b
Vilovinto <vladvierboviy@gmail.com>
2024-10-21 11:42:47 +0300

    Homework3

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+let routes = createBrowserRouter([]);
+
 root.render(
-    <App/>
+    <RouterProvider router={routes}/>
 );
